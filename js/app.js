40840e87d2832b854eb2aca8dcfe5e4d10dbdda4
PhiliciaWatson <philiciawatson7@gmail.com>
2020-07-10 05:30:58 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+//Task1
+var cake = [];
+for (var i = 0; i <= 10; i++) {
+  cake.push(i);
+}
+console.log(cake);
+
+//Task2
+var love = [];
+var i = 0;
+while(i <= 10) {
+  love.push(i);
+  i--;
+}
+console.log(love);
+
+//Task3
+var cat = [];
+for (var i = 11; i <= 21; i += 2) {
+  cat.push(i);
+}
+console.log(cat)
+
+//Task5
+var sing = [];
+for (var i = 0; i >= 10; i -= 1) {
+  sing.push(i);
+}
+console.log(sing)
